b6fd7456ef043139b3598b7777b2f1e2b4601bda
SaraPe <135864591+sarap29@users.noreply.github.com>
2023-08-24 19:41:54 +0000

    se realiza ejercicio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = function generadorDeCartas() {
+  let numeroCarta = document.querySelector("#numerodecarta");
+  let paloArriba = document.querySelector("#paloarriba");
+  let paloAbajo = document.querySelector("#paloabajo");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  function generadorPaloCarta() {
+    let paloCarta = ["♦", "♥", "♠", "♣"];
+    let paloCartaIndex = Math.floor(Math.random() * paloCarta.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let carta = paloCarta[paloCartaIndex];
+    if (carta === "♦" || carta === "♥") {
+      carta = `<span style="color: red;">${carta}</span>`;
+    }
+    return carta;
+  }
+
+  let palo = generadorPaloCarta();
+  paloArriba.innerHTML = palo;
+  paloAbajo.innerHTML = palo;
+
+  function generadorNumeroCarta() {
+    let valoresCarta = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    let valoresCartaIndex = Math.floor(Math.random() * valoresCarta.length);
+    return valoresCarta[valoresCartaIndex];
+  }
+
+  let numero = generadorNumeroCarta();
+  numeroCarta.innerHTML = numero;
+
+  //Botón
+  let botonCartaNueva = document.getElementById("btn");
+  botonCartaNueva.addEventListener("click", generadorDeCartas);
 };
